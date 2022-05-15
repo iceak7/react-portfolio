9360db4c5fb17f0c2ed063a3e57355c5c213c4b6
Isak <isak.jensen@telia.com>
2022-05-15 17:11:50 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [name, setName] = useState([]);
+  useEffect(()=>{
+      fetch('https://api.github.com/users/iceak7/repos')
+      .then(res=>res.json())
+      .then(data =>{
+          setName(data)
+      })
+  },[]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h1 className="pageTitle">Portfolio</h1>
+      <div className="portfolioMain">
+
+      {name.map((names, index)=>(
+        <>
+
+        <div key={index}>
+        <div  className="portfolioItem">
+          <div className="portoflioImageContainer">
+            <img src="github-logo.png" alt="FPL Logo"/>
+          </div>
+          <div className="portfolioInfo">
+            <h2>{names.name}</h2>
+            <a className="button" href={'#popup'+index}>Mer Info</a>
+          </div>
+        </div>
+
+        <div id={'popup'+index} className="overlay">
+          <div className="popup">
+            <h2>{names.name}</h2>
+            <a className="close" href="#">&times;</a>
+            <div className="content">
+              {names.description===null ? 'Ingen beskrivning finns för det här projektet.' : names.description}
+              <p><a href={names.html_url}>Länk</a></p>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        </>
+        ))}
+        </div>
     </div>
   );
 }
